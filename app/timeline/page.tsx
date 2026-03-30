91a0945ebcfe5937a6be1ee0316c072deaca25de
logicneuron0 <kashelkarkunal0@gmail.com>
2026-03-31 01:44:31 +0530

    merge timeline page

---
+++ app/timeline/page.tsx
@@ -0,0 +1,24 @@
+import BorderlandTimeline from "../components/timeline";
+import HybridDomainPage from "../components/domain";
+import PrizeSection from "../components/prize_page";
+
+export default function MainGamePage() {
+  return (
+    <main className="bg-[#050508] relative flex flex-col w-screen overflow-x-hidden">
+      {/* 1. Timeline Section */}
+      <div className="w-full relative z-10 block">
+        <BorderlandTimeline />
+      </div>
+
+      {/* 2. Domains Section */}
+      <div className="w-full relative z-20 block bg-black">
+        <HybridDomainPage />
+      </div>
+
+      {/* 3. Prize Section */}
+      <div className="w-full relative z-30 block bg-black">
+        <PrizeSection />
+      </div>
+    </main>
+  );
+}
